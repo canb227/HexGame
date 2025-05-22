e5a3d9b1322ccd9965666c8a1c5068b6537ded63
Stephen Banghart <stephen.banghart@outlook.com>
2025-05-22 14:35:33 -0400

    fixing FP lib import

--- Global.cs
+++ Global.cs
@@ -5,14 +5,44 @@ using Steamworks;
 
 public partial class Global : Node
 {
+    //Steam stuff
+    public static uint STEAM_APP_ID = 480;
+    public static ulong steamID = 0;
+
+    //Abusing singletons
+    public static Global instance;
+
+    //Register global variables here with "public static"
+    //If the compiler gives you trouble with Global.[varname], try using Global.instance.[varname]
     public static Layout layout;
     public static Camera camera;
 
-    public static Global instance;
 
+    //This ready codeblock is the first non-engine code to run anywhere in the game, since Global is autoloaded by Godot before anything else
     public override void _Ready()
     {
+        //also abusing singletons
         Global instance = this;
-        SteamClient.Init(480, true);
+
+        SteamInit();
+    }
+
+    public void SteamInit()
+    {
+        SteamClient.Init(STEAM_APP_ID, true);
+        if (SteamClient.IsValid)
+        {
+            steamID = SteamClient.SteamId.Value;
+            GD.Print("Steam ID: " + steamID);
+        }
+        else
+        {
+            GD.Print("Steam not initialized");
+        }
+    }
+
+    public void Log(string message)
+    {
+        GD.Print(message);
     }
 }
