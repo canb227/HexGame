6bb1495a6314cfc3fc1326ea4d5e18e3cf60563e
Stephen Banghart <stephen.banghart@outlook.com>
2025-07-29 14:03:44 -0400

    fixed hex equals

--- hex/Hex.cs
+++ hex/Hex.cs
@@ -241,17 +241,6 @@ public List<Hex> WrappingRange(int range, int left, int right, int top, int bott
         return this.Distance(unwrappedHex);
     }
 
-    public override bool Equals([NotNullWhen(true)] object obj)
-    {
-        if (obj is Hex h)
-        {
-            if (h.r == this.r && h.q == this.q && h.s == this.s)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
 }
 
 public struct FractionalHex
