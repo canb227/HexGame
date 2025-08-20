a0f91fe011f5b090a2c1c5d0fc7569bc2a5c35a7
jeffercizer <jeffercize@gmail.com>
2025-08-20 15:44:36 -0400

    minor fix

--- graphics/GraphicUnit.cs
+++ graphics/GraphicUnit.cs
@@ -73,8 +73,6 @@ public partial class GraphicUnit : GraphicObject
     private void InstantiateUnit(Unit unit)
     {
         UnitLoader.unitsDict.TryGetValue(unit.unitType, out UnitInfo unitInfo);
-        GD.Print(unit.name);
-        GD.Print(unitInfo.ModelPaths.Any());
         node3D = Godot.ResourceLoader.Load<PackedScene>("res://" + unitInfo.ModelPaths[Global.gameManager.game.playerDictionary[unit.teamNum].faction]).Instantiate<Node3D>();
         Transform3D newTransform = node3D.Transform;
         GraphicGameBoard ggb = (GraphicGameBoard)(Global.gameManager.graphicManager.graphicObjectDictionary[Global.gameManager.game.mainGameBoard.id]);
